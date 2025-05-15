3a995c94d4dbe6995dec66d37459b4253d077e72
lanmoya518 <lanmoya518@gmail.com>
2025-05-15 14:17:58 +0900

    update: 新增评论区功能，footer追加rss订阅按钮

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+// import rss from "@astrojs/rss";
 
 // https://astro.build/config
 export default defineConfig({
+  // integrations: [rss()],
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://curly-sheep.netlify.app/"
+  site: "https://curly-sheep.netlify.app/",
 });
