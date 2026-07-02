be199c4b5c491aeb54911412c69fc95e75eec57c
lanmoya <lanmoya518@gmail.com>
2026-07-02 12:28:18 +0900

    fix: 一系列修改

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,6 @@
 import { z, defineCollection } from "astro:content";
 import { glob } from 'astro/loaders';
 
-const projectsCollection = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/projects" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string()
-    }),
-    worksImage1: z.object({
-      url: z.string(),
-      alt: z.string()
-    }),
-    worksImage2: z.object({
-      url: z.string(),
-      alt: z.string()
-    }),
-    platform: z.string(),
-    stack: z.string(),
-    website: z.string(),
-    github: z.string(),
-  })
-});
 
 const postsCollection = defineCollection({
   schema: z.object({
@@ -44,6 +21,5 @@ const postsCollection = defineCollection({
 
 
 export const collections = {
-  projects: projectsCollection,
   posts: postsCollection
 };
