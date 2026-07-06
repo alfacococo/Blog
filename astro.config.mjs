da019c12dc45bf448e316f60a205c6f8d2b59706
lanmoya <lanmoya518@gmail.com>
2026-07-06 09:21:23 +0900

    update: 博文的外部链接自动在新标签页打开

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import { rehypeLazyImages } from "./lazy-images.mjs";
+import rehypeExternalLinks from 'rehype-external-links';
 import tailwindcss from "@tailwindcss/vite";
 // import rss from "@astrojs/rss";
 
@@ -10,7 +11,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   markdown: {
-    rehypePlugins: [rehypeLazyImages],
+    rehypePlugins: [rehypeLazyImages, [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]],
   },
   site: "https://curly-sheep.netlify.app/",
 });
