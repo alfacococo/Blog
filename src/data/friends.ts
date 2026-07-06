1a84d9e546f46268cfdba5c078e41978f6f92275
lanmoya <lanmoya518@gmail.com>
2026-07-07 06:51:58 +0900

    fix: 友情链接单独拆分，给首页瘦身

---
+++ src/data/friends.ts
@@ -0,0 +1,74 @@
+export interface Friend {
+  name: string;
+  url: string;
+  motto: string;
+}
+
+export const friends: Friend[] = [
+  {
+    name: '天堂错误文件',
+    url: 'https://naturaleki.one/',
+    motto: "Across the street, cross one's heart.",
+  },
+  {
+    name: '猫头气球生产工厂',
+    url: 'https://ccaatthouse.icu',
+    motto: '我们不生产猫头气球',
+  },
+  {
+    name: '此地有野狗出没',
+    url: 'https://luoshui.icu/',
+    motto: '小心！野兽凶猛！',
+  },
+  {
+    name: '树响集',
+    url: 'https://wbzddwsw.vercel.app/',
+    motto: '不作诗则做诗',
+  },
+  {
+    name: '漫游者迷路中',
+    url: 'https://blog-sooty-beta.vercel.app/',
+    motto: '记生活迷思，做吟游诗人',
+  },
+  {
+    name: 'AI碎片缓存',
+    url: 'https://hirate3.wordpress.com/',
+    motto: '欢迎访问虚拟仓库',
+  },
+  {
+    name: '晴空',
+    url: 'https://summerwasover.vercel.app/',
+    motto: '像鸟儿编辑它们的飞行一般书写',
+  },
+  {
+    name: 'Meaningless Nothingness',
+    url: 'https://xyzxy.me',
+    motto: '桂棹兮兰桨，击空明兮溯流光',
+  },
+  { name: '夏小雪的星球散步', url: 'https://sizheng.org/', motto: '' },
+  { name: '蝴蝶曾在此处', url: 'https://changingmoments.one/', motto: '' },
+  {
+    name: '深海鱼拒绝触礁',
+    url: 'https://trails-of-isara.vercel.app/',
+    motto: '把最棒的吊桥效应献给你',
+  },
+  { name: '山月', url: 'https://sanguok.com', motto: '左海逐月，雙峰遺夢' },
+  { name: '42th Window', url: 'https://blog.welsea.site/', motto: '' },
+  { name: '稽异录残篇', url: 'https://ocod.netlify.app/', motto: '' },
+    
+  {
+    name: '日々精進',
+    url: 'https://arayr.com/',
+    motto: '一般会社员A的电子手帐。',
+  },
+  {
+    name: '小球飞鱼',
+    url: 'https://mantyke.icu/',
+    motto: '我们会一起遇见鲸鱼吗？',
+  },
+    {
+    name: "Mimoshark's Blog",
+    url: 'https://notion-next-neon-eight.vercel.app/',
+    motto: '',
+  },
+];
