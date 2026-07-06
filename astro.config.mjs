d631f42aa348e45934561aaae5ba6ee7b6e9a477
lanmoya <lanmoya518@gmail.com>
2026-07-07 07:11:24 +0900

    fix: 删掉不用的代码/依赖，精确明明social-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import { rehypeLazyImages } from "./lazy-images.mjs";
 import rehypeExternalLinks from 'rehype-external-links';
 import tailwindcss from "@tailwindcss/vite";
-// import rss from "@astrojs/rss";
 
 // https://astro.build/config
 export default defineConfig({
-  // integrations: [rss()],
   vite: {
     plugins: [tailwindcss()],
   },
